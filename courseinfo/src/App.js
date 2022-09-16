7449f1ba765ac17e62cd974208992422f253f87e
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2022-09-16 19:10:58 +0500

    Exercise 1.4 - course information

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -11,39 +11,41 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.part1.name} exercises={props.part1.exercises}/>
-      <Part name={props.part2.name} exercises={props.part2.exercises}/>
-      <Part name={props.part3.name} exercises={props.part3.exercises}/>
+      {props.parts.map((part) => <Part key={part.name} name={part.name} exercises={part.exercises}/>)}
     </div>
   );
 }
 
 const Total = (props) => {
+  // I kept it simple and avoided using Array.reduce
+  const [part1, part2, part3] = props.parts;
   return (
-    <p>Number of exercises {props.exercises}</p>
+    <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
   );
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
